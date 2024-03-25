44042e4db579a87b576a73b25ba13a12b5a3db95
Ebenezer Ametepeh <eddiefestde@gmail.com>
2024-03-25 22:21:33 +0000

    changes added

---
+++ part07/app/page.jsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <>
+      {/* // Header Section */}
+      <header className="">Hello</header>
+      {/* // Header Section End*/}
+
+      {/* // Main Section End */}
+      <main className="">hola</main>
+      {/* // Main Section End */}
+
+      {/* Footer Section */}
+      <footer className=""></footer>
+      {/* Footer Section End */}
+    </>
+  );
+}
